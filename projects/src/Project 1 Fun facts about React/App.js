9f14230414a2d99e238230dcc41dc0b67f3959ed
Abdallah Sow <abdallahsow700@gmail.com>
2022-08-30 13:11:58 +0300

    project setup

--- projects/src/Project 1 Fun facts about React/App.js
+++ projects/src/Project 1 Fun facts about React/App.js
@@ -1,18 +1,29 @@
 import './App.css';
-import logo from './react-logo.png';
+// import logo from './react-logo.png';
+import Navbar from './Components/Navbar';
+import Main from './Components/Main';
 
-const StaticPage = () => (
+// const StaticPage = () => (
+//   <div>
+//     <img className="react-logo" src={logo} alt="" />
+//     <h1>Fun facts about React</h1>
+//     <ul>
+//       <li>Was first released in 2013</li>
+//       <li>Was originally created by Jordan Walke</li>
+//       <li>Has well over 100k stars on GitHub</li>
+//       <li>Is maintained by Facebook</li>
+//       <li>Powers thousands of entreprise apps, including mobile apps</li>
+//     </ul>
+//   </div>
+// );
+
+// export default StaticPage;
+
+const App = () => (
   <div>
-    <img className="react-logo" src={logo} alt="" />
-    <h1>Fun facts about React</h1>
-    <ul>
-      <li>Was first released in 2013</li>
-      <li>Was originally created by Jordan Walke</li>
-      <li>Has well over 100k stars on GitHub</li>
-      <li>Is maintained by Facebook</li>
-      <li>Powers thousands of entreprise apps, including mobile apps</li>
-    </ul>
+    <Navbar />
+    <Main />
   </div>
-);
+)
 
-export default StaticPage;
+export default App;
