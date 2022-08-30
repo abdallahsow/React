08db02f424da3a8612b370ad6aa4032ed7fcb270
Abdallah Sow <abdallahsow700@gmail.com>
2022-08-30 14:17:39 +0300

    completed Navbar component

--- projects/src/Project 1 Fun facts about React/Components/Main.js
+++ projects/src/Project 1 Fun facts about React/Components/Main.js
@@ -1,3 +1,14 @@
 export default function Main() {
-    return <h1>Main goes here!</h1>
+    return (
+      <main className="main-body">
+        <h1 className="h1-title">Fun facts about React</h1>
+        <ul>
+          <li>Was first released in 2013</li>
+          <li>Was originally created by Jordan Walke</li>
+          <li>Has well over 100k stars on GitHub</li>
+          <li>Is maintained by Facebook</li>
+          <li>Powers thousands of entreprise apps, including mobile apps</li>
+        </ul>
+      </main>
+    );
 }
--- projects/src/Project 1 Fun facts about React/Components/Navbar.js
+++ projects/src/Project 1 Fun facts about React/Components/Navbar.js
@@ -1,3 +1,13 @@
+import logo from "./react-logo.png";
+
 export default function Navbar() {
-    return <h1>Navbar goes here!</h1>
+  return (
+    <nav className="navbar">
+      <div className="main-logo">
+        <img className="react-logo" src={logo} alt="" />
+        <h3 className="logo-title">ReactFacts</h3>
+      </div>
+      <h4 className="extra-title">React Course - Project 1</h4>
+    </nav>
+  );
 }
