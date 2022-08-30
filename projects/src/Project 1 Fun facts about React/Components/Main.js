31de8a1b5b2b8230ebce048da0ae38fbfb8594e4
Abdallah Sow <abdallahsow700@gmail.com>
2022-08-30 15:17:23 +0300

    completed the main section

--- projects/src/Project 1 Fun facts about React/Components/Main.js
+++ projects/src/Project 1 Fun facts about React/Components/Main.js
@@ -1,14 +1,14 @@
 export default function Main() {
-    return (
-      <main className="main-body">
-        <h1 className="h1-title">Fun facts about React</h1>
-        <ul>
-          <li>Was first released in 2013</li>
-          <li>Was originally created by Jordan Walke</li>
-          <li>Has well over 100k stars on GitHub</li>
-          <li>Is maintained by Facebook</li>
-          <li>Powers thousands of entreprise apps, including mobile apps</li>
-        </ul>
-      </main>
-    );
+  return (
+    <main className="main-body">
+      <h1 className="h1-title">Fun facts about React</h1>
+      <ul className="facts">
+        <li className="body-text">Was first released in 2013</li>
+        <li className="body-text">Was originally created by Jordan Walke</li>
+        <li className="body-text">Has well over 100k stars on GitHub</li>
+        <li className="body-text">Is maintained by Facebook</li>
+        <li className="body-text">Powers thousands of entreprise apps, including mobile apps</li>
+      </ul>
+    </main>
+  );
 }
