fa7198b1044571515df9a385823286a5f25f1fc9
Abdallah Sow <abdallahsow700@gmail.com>
2022-09-02 12:12:10 +0300

    updated the Navbar and Pictures' components

--- projects/src/project-2-airbnb-experiences-clone/App.js
+++ projects/src/project-2-airbnb-experiences-clone/App.js
@@ -1,10 +1,14 @@
-import './App.css'
-import HeroSection from './components/HeroSection'
+import "./App.css";
+import { Navbar, Pictures, Advertisement } from "./components/HeroSection";
 
 const App = () => (
-    <div className='app'>
-        <HeroSection />
+  <main className="app">
+    <Navbar />
+    <div className="experiences">
+      <Pictures />
+      <Advertisement />
     </div>
-)
+  </main>
+);
 
 export default App;
--- projects/src/project-2-airbnb-experiences-clone/components/HeroSection.js
+++ projects/src/project-2-airbnb-experiences-clone/components/HeroSection.js
@@ -1,34 +1,62 @@
-import AirBnB from '../images/airbnb-logo.png';
-import Cooker from '../images/cooker.png';
-import Worker from '../images/worker.png';
-import Balina from '../images/balina.png';
-import Food from '../images/food.png';
-import Guitar from '../images/guitar.png';
+import AirBnB from "../images/airbnb-logo.png";
+import Cooker from "../images/cooker.png";
+import Worker from "../images/worker.png";
+import Balina from "../images/balina.png";
+import Food from "../images/food.png";
+import Guitar from "../images/guitar.png";
 import Piano from "../images/piano.png";
 import Swimmer from "../images/swimmer.png";
 import Yoga from "../images/yoga.png";
 import Singer from "../images/singer.png";
 
-function HeroSection() {
-    return (
-      <div className="hero-section">
-        <nav className="logo">
-          <img src={AirBnB} alt="" />
-        </nav>
-        <section>
-          <img src={Cooker} alt="" />
+const Navbar = function () {
+  return (
+    <nav className="navbar">
+      <img src={AirBnB} alt="" />
+    </nav>
+  );
+};
+
+function Pictures() {
+  return (
+    <section>
+      <div className="pictures-section">
+        <img src={Cooker} alt="" />
+        {/* Group 1 */}
+        <div className="group-style-1">
           <img src={Worker} alt="" />
           <img src={Balina} alt="" />
+        </div>
+        {/* Group 2 */}
+        <div className="group-style-2">
           <img src={Food} alt="" />
           <img src={Guitar} alt="" />
+        </div>
+        {/* Group 3 */}
+        <div className="group-style-3">
           <img src={Piano} alt="" />
           <img src={Swimmer} alt="" />
+        </div>
+        {/* Group 4 */}
+        <div className="group-style-4">
           <img src={Yoga} alt="" />
           <img src={Singer} alt="" />
-        </section>
+        </div>
       </div>
-    );
-    
+    </section>
+  );
 }
 
-export default HeroSection;
+function Advertisement() {
+  return (
+    <div>
+      <h1 className="advert-title">Online Experiences</h1>
+      <p className="advert-p">
+        Join unique interactive activities led by one-of-a-kind hosts—all
+        without leaving home.
+      </p>
+    </div>
+  );
+}
+
+export { Navbar, Pictures, Advertisement };
