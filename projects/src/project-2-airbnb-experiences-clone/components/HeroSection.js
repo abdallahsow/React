540ad653b85ed2567742b2b6b05ab72e3a9a4de4
Abdallah Sow <abdallahsow700@gmail.com>
2022-09-02 10:39:48 +0300

    updated the images folder with more picture

--- projects/src/project-2-airbnb-experiences-clone/components/HeroSection.js
+++ projects/src/project-2-airbnb-experiences-clone/components/HeroSection.js
@@ -1,15 +1,33 @@
 import AirBnB from '../images/airbnb-logo.png';
-
-const Navbar = () => (
-    <img className="logo" src={AirBnB} alt="" />
-)
+import Cooker from '../images/cooker.png';
+import Worker from '../images/worker.png';
+import Balina from '../images/balina.png';
+import Food from '../images/food.png';
+import Guitar from '../images/guitar.png';
+import Piano from "../images/piano.png";
+import Swimmer from "../images/swimmer.png";
+import Yoga from "../images/yoga.png";
+import Singer from "../images/singer.png";
 
 function HeroSection() {
     return (
-        <div className="hero-section">
-        <Navbar />
-    </div>
-    )
+      <div className="hero-section">
+        <nav className="logo">
+          <img src={AirBnB} alt="" />
+        </nav>
+        <section>
+          <img src={Cooker} alt="" />
+          <img src={Worker} alt="" />
+          <img src={Balina} alt="" />
+          <img src={Food} alt="" />
+          <img src={Guitar} alt="" />
+          <img src={Piano} alt="" />
+          <img src={Swimmer} alt="" />
+          <img src={Yoga} alt="" />
+          <img src={Singer} alt="" />
+        </section>
+      </div>
+    );
     
 }
 
