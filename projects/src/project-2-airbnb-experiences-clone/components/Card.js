6933012ffea13e89c138d8e468c8e3161f38636c
Abdallah Sow <abdallahsow700@gmail.com>
2022-09-03 22:26:39 +0300

    updated the css

--- projects/src/project-2-airbnb-experiences-clone/components/Card.js
+++ projects/src/project-2-airbnb-experiences-clone/components/Card.js
@@ -7,7 +7,7 @@ export default function Card() {
       <img className="cards--pic" src={Athlete} alt="Olympic Swimmer" />
       <div>
         <img src={Star} alt="" />
-          <p className="go">
+          <p className="cards--text">
             5.0 <span>(6) USA</span>
           </p>
       </div>
