b1727df2d222213e9f5800686383607769be2dbe
Abdallah Sow <abdallahsow700@gmail.com>
2022-09-05 10:34:35 +0300

    created and styled the first card component

---
+++ projects/src/practice-2/contact/App.js
@@ -0,0 +1,10 @@
+import Felix from "./components/Felix";
+import "./App.css";
+
+const App = () => (
+  <main className="app">
+    <Felix />
+  </main>
+);
+
+export default App;
---
+++ projects/src/practice-2/contact/components/Felix.js
@@ -0,0 +1,22 @@
+import felixcat from "../images/felix.png";
+import Phone from "../images/phone-icon.png";
+import Email from "../images/mail-icon.png";
+
+const Felix = () => (
+  <div className="contacts">
+    <div className="contact-card">
+      <img className="client-pic" src={felixcat} alt="Felix the Cat" />
+      <h2 className="client">Felix</h2>
+      <div className="info-group">
+        <img className="info-img" src={Phone} alt="Felix's phone number" />
+        <p className="info-text">(212) 555-4567</p>
+      </div>
+      <div className="info-group">
+        <img className="info-img" src={Email} alt="Felix's email adress" />
+        <p className="info-text">thecat@hotmail.com</p>
+      </div>
+    </div>
+  </div>
+);
+
+export default Felix;
