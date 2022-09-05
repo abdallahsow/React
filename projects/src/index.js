e97c339e9df500e65f194e4ff15a97ca3cdd68c6
Abdallah Sow <abdallahsow700@gmail.com>
2022-09-05 14:48:30 +0300

    added a new quiz

--- projects/src/index.js
+++ projects/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from "./practice-2/contact/App";
+import App from "./practice-2/hard-coded-contact/App";
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
