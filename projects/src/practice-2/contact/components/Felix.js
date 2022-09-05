60ef5a71c32b559774a06bf1c231204941befb92
Abdallah Sow <abdallahsow700@gmail.com>
2022-09-05 10:41:19 +0300

    added more cards in the Felix component

--- projects/src/practice-2/contact/components/Felix.js
+++ projects/src/practice-2/contact/components/Felix.js
@@ -4,6 +4,49 @@ import Email from "../images/mail-icon.png";
 
 const Felix = () => (
   <div className="contacts">
+    {/* Card 1 */}
+    <div className="contact-card">
+      <img className="client-pic" src={felixcat} alt="Felix the Cat" />
+      <h2 className="client">Felix</h2>
+      <div className="info-group">
+        <img className="info-img" src={Phone} alt="Felix's phone number" />
+        <p className="info-text">(212) 555-4567</p>
+      </div>
+      <div className="info-group">
+        <img className="info-img" src={Email} alt="Felix's email adress" />
+        <p className="info-text">thecat@hotmail.com</p>
+      </div>
+    </div>
+
+    {/* Card 2 */}
+    <div className="contact-card">
+      <img className="client-pic" src={felixcat} alt="Felix the Cat" />
+      <h2 className="client">Felix</h2>
+      <div className="info-group">
+        <img className="info-img" src={Phone} alt="Felix's phone number" />
+        <p className="info-text">(212) 555-4567</p>
+      </div>
+      <div className="info-group">
+        <img className="info-img" src={Email} alt="Felix's email adress" />
+        <p className="info-text">thecat@hotmail.com</p>
+      </div>
+    </div>
+
+    {/* Card 3 */}
+    <div className="contact-card">
+      <img className="client-pic" src={felixcat} alt="Felix the Cat" />
+      <h2 className="client">Felix</h2>
+      <div className="info-group">
+        <img className="info-img" src={Phone} alt="Felix's phone number" />
+        <p className="info-text">(212) 555-4567</p>
+      </div>
+      <div className="info-group">
+        <img className="info-img" src={Email} alt="Felix's email adress" />
+        <p className="info-text">thecat@hotmail.com</p>
+      </div>
+    </div>
+
+    {/* Card 4 */}
     <div className="contact-card">
       <img className="client-pic" src={felixcat} alt="Felix the Cat" />
       <h2 className="client">Felix</h2>
