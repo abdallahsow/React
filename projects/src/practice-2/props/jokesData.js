e570368151edbf8b2307eb06cfbd41dcc3376211
Abdallah Sow <abdallahsow700@gmail.com>
2022-09-07 11:11:36 +0300

    added an array of data in a js file

---
+++ projects/src/practice-2/props/jokesData.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    setup: "What did the fish say when it hit the wall?",
+    punchline: "Dam.",
+    upvotes: 20,
+    downvotes: 10,
+    isPun: true,
+  },
+  {
+    setup: "I'm reading a book about anti-gravity...",
+    punchline: "It's impossible to put down.",
+    upvotes: 10,
+    downvotes: 20,
+    isPun: false,
+  },
+  {
+    setup: "What do you call a fake noodle?",
+    punchline: "An Impasta.",
+    upvotes: 10,
+    downvotes: 20,
+    isPun: true,
+  },
+  {
+    setup: "How many apples grow on a tree?",
+    punchline: "All of them.",
+    upvotes: 10,
+    downvotes: 20,
+    isPun: true,
+  },
+  {
+    setup: "Want to hear a joke about paper?",
+    punchline: "Nevermind it's tearable.",
+    upvotes: 18,
+    downvotes: 40,
+    isPun: true,
+  },
+];
