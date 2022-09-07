4589e4267ffc161ffd971ee5142b5d9af953ee87
Abdallah Sow <abdallahsow700@gmail.com>
2022-09-07 14:11:19 +0300

    added more data for the next cards

--- projects/src/project-2-airbnb-experiences-clone/components/data.js
+++ projects/src/project-2-airbnb-experiences-clone/components/data.js
@@ -1,16 +1,36 @@
 import Athlete from "../images/zaferes-usa.png";
 import Star from "../images/star-icon.png";
+import Hiking from "../images/mountain-bike.png";
+import Maid from "../images/wedding.png";
 
 const data = [
-    {
-        img: Athlete,
-        star: Star,
-        rating: 5.0,
-        reviews: 6,
-        location: "USA",
-        title: "Life lessons with Katie Zaferes",
-        price: 136
-    }   
+  {
+    img: Athlete,
+    star: Star,
+    rating: 5.0,
+    reviews: 6,
+    location: "USA",
+    title: "Life lessons with Katie Zaferes",
+    price: 136,
+  },
+  {
+    img: Maid,
+    star: Star,
+    rating: 5.0,
+    reviews: 30,
+    location: "USA",
+    title: "Learn Wedding Photography",
+    price: 125,
+  },
+  {
+    img: Hiking,
+    star: Star,
+    rating: 4.8,
+    reviews: 2,
+    location: "USA",
+    title: "Group Mountain Biking",
+    price: 50,
+  }
 ];
 
 export default data;
