30ed6b70229327d9ab17ad18838d88e8dbef6c72
Abdallah Sow <abdallahsow700@gmail.com>
2022-09-09 12:23:50 +0300

    created the MemeInput component

--- projects/src/project-3-meme-generator/App.js
+++ projects/src/project-3-meme-generator/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import MemeInput from "./components/MemeInput";
 
 const App = () => (
   <div className="app">
     <Navbar />
+    <MemeInput />
   </div>
 );
 
---
+++ projects/src/project-3-meme-generator/components/MemeInput.js
@@ -0,0 +1,11 @@
+const MemeInput = () => (
+  <div className="meme-input">
+    <div className="input--fields">
+      <input className="input-field" />
+      <input className="input-field" />
+    </div>
+    <button className="meme-button">Get a new meme image</button>
+  </div>
+);
+
+export default MemeInput;
