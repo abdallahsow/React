b0edc837e45520424c7d1b61776fa99cdbd545b9
Abdallah Sow <abdallahsow700@gmail.com>
2022-09-09 14:36:56 +0300

    updated the GeneratedMeme component

--- projects/src/project-3-meme-generator/App.js
+++ projects/src/project-3-meme-generator/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import MemeInput from "./components/MemeInput";
+import GeneratedMeme from "./components/GeneratedMeme";
 
 const App = () => (
   <div className="app">
     <Navbar />
-    <MemeInput />
+    <section className="meme-section">
+      <MemeInput />
+      <GeneratedMeme />
+    </section>
   </div>
 );
 
