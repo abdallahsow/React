25ffa210a2f80a6da9997c6db9a339c7518049e0
Abdallah Sow <abdallahsow700@gmail.com>
2022-09-10 20:20:16 +0300

    removed the GeneratedMeme content to MemeInput

--- projects/src/project-3-meme-generator/components/MemeInput.js
+++ projects/src/project-3-meme-generator/components/MemeInput.js
@@ -1,10 +1,19 @@
+import Joker from "../images/troller-face.png";
+
 const MemeInput = () => (
-  <div className="meme-input">
-    <div className="input--fields">
-      <input className="input-field" />
-      <input className="input-field" />
+  <div>
+    <div className="meme-input">
+      <div className="input--fields">
+        <input className="input-field" />
+        <input className="input-field" />
+      </div>
+      <button className="meme-button">Get a new meme image</button>
     </div>
-    <button className="meme-button">Get a new meme image</button>
+    <div>
+      <div></div>
+      <img src={Joker} alt="meme" />
+      <div></div>
+    </div>
   </div>
 );
 
