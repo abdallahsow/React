4914c80af552d5a47b62e0acca6a45eb12e04cce
Abdallah Sow <abdallahsow700@gmail.com>
2022-09-10 20:44:59 +0300

    Rendered in the index.js

--- projects/src/project-3-meme-generator/App.js
+++ projects/src/project-3-meme-generator/App.js
@@ -1,14 +1,12 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import MemeInput from "./components/MemeInput";
-import GeneratedMeme from "./components/GeneratedMeme";
 
 const App = () => (
   <div className="app">
     <Navbar />
     <section className="meme-section">
       <MemeInput />
-      <GeneratedMeme />
     </section>
   </div>
 );
