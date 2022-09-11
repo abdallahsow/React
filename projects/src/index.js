6b936608a57c618d0d7dcb1af492e5498a3b91a3
Abdallah Sow <abdallahsow700@gmail.com>
2022-09-11 17:44:28 +0300

    added a the states folder in practice-3

--- projects/src/index.js
+++ projects/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from "./practice-3/adding-item/add-item";
+import UseState from './practice-3/states/Using-state';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
-root.render(<App />);
+root.render(<UseState />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ projects/src/practice-3/states/Using-state.js
@@ -0,0 +1,14 @@
+import './state.css'
+
+const UseState = () => {
+  return (
+    <div className="state">
+      <h1 className="state-title">Is state important to know?</h1>
+      <div className="state-value">
+        <h3>Yes</h3>
+      </div>
+    </div>
+  );
+};
+
+export default UseState;
