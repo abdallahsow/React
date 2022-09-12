5fe6c9dfa5c65f337b0f3335df5d3e5abc9f5795
Abdallah Sow <abdallahsow700@gmail.com>
2022-09-12 16:49:55 +0300

    used state in order to render the meme image locally in a updated way

--- projects/src/index.js
+++ projects/src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Count from "./practice-3/states/Using-state";
+import App from "./project-3-meme-generator/App";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Count />);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- projects/src/project-3-meme-generator/components/MemeInput.js
+++ projects/src/project-3-meme-generator/components/MemeInput.js
@@ -1,29 +1,37 @@
-import Joker from "../images/troller-face.png";
-import memesData from "./memesData"
+import memesData from "./memesData";
+import React, { useState } from "react";
 
 const MemeInput = () => {
-  function getMeme() {
-    const { data: { memes } } = memesData;
-    const randomNumber = Math.floor(Math.random() * memes.length);
-    const imgUrl = memes[randomNumber].url
-    console.log(imgUrl)
-  }
+  const [memeImg, setMemeImg] = useState();
   
+  function getMeme() {
+    const {
+      data: { memes },
+    } = memesData;
+    const randomNumber = Math.floor(Math.random() * memes.length);
+    const imgUrl = memes[randomNumber].url;
+    console.log(imgUrl);
+    setMemeImg(imgUrl);
+  }
+
   return (
     <div>
-    <div className="meme-input">
-      <div className="input--fields">
-        <input className="input-field" />
-        <input className="input-field" />
+      <div className="meme-input">
+        <div className="input--fields">
+          <input className="input-field" />
+          <input className="input-field" />
+        </div>
+        <button onClick={getMeme} className="meme-button">
+          Get a new meme image
+        </button>
       </div>
-      <button onClick={getMeme} className="meme-button">Get a new meme image</button>
+      <div>
+        <div></div>
+        <img className="meme-pic" src={memeImg} alt="meme" />
+        <div></div>
+      </div>
     </div>
-    <div>
-      <div></div>
-      <img src={Joker} alt="meme" />
-      <div></div>
-    </div>
-  </div>
-)};
+  );
+};
 
 export default MemeInput;
