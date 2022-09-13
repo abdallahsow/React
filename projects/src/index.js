e9edc0de8c39f690a9e73322156128a2c6ae0bf5
Abdallah Sow <abdallahsow700@gmail.com>
2022-09-13 14:21:01 +0300

    created the App component in objects-state and updated it

--- projects/src/index.js
+++ projects/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./practice-3/adding-item/Add-item";
+import App from "./practice-3/complex-state-objects/objects-state";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
--- projects/src/practice-3/complex-state-objects/objects-state.js
+++ projects/src/practice-3/complex-state-objects/objects-state.js
@@ -1,9 +1,19 @@
 import './app.css'
+import User from './images/user.png'
+import emptyStar from './images/star-empty.png'
 
 const App = () => {
     return (
-        <article>
-            
-        </article>
-    )
+      <article className="user">
+        <img className="user-pic" src={User} alt="user" />
+        <div>
+          <img className="user-star" src={emptyStar} alt="star" />
+          <h3 className="title">John Doe</h3>
+          <p className="text">+ 1 (719) 555-1212</p>
+          <p className="text">itsmyrealname@example.com</p>
+        </div>
+      </article>
+    );
 }
+
+export default App
