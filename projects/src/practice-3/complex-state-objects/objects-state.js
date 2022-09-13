ccf08a56f6a01fba924f05412092f31301f1bff8
Abdallah Sow <abdallahsow700@gmail.com>
2022-09-13 14:30:20 +0300

    filled the markup using the value defined by the object state

--- projects/src/practice-3/complex-state-objects/objects-state.js
+++ projects/src/practice-3/complex-state-objects/objects-state.js
@@ -1,19 +1,38 @@
-import './app.css'
-import User from './images/user.png'
-import emptyStar from './images/star-empty.png'
+import "./app.css";
+import User from "./images/user.png";
+import emptyStar from "./images/star-empty.png";
+import React from 'react';
 
 const App = () => {
-    return (
-      <article className="user">
-        <img className="user-pic" src={User} alt="user" />
-        <div>
-          <img className="user-star" src={emptyStar} alt="star" />
-          <h3 className="title">John Doe</h3>
-          <p className="text">+ 1 (719) 555-1212</p>
-          <p className="text">itsmyrealname@example.com</p>
-        </div>
-      </article>
-    );
-}
+  const [contact, setContact] = React.useState({
+    firstName: "John",
+    lastName: "Doe",
+    phone: "+1 (719) 555-1212",
+    email: "itsmyrealname@example.com",
+  });
 
-export default App
+  const toggleFavorite = () => {
+    console.log("Toggle Favorite");
+  };
+
+  return (
+    <article className="user">
+      <img className="user-pic" src={User} alt="user" />
+      <div>
+        <img
+          className="user-star"
+          src={emptyStar}
+          alt="star"
+          onClick={toggleFavorite}
+        />
+        <h3 className="title">
+          {contact.firstName} {contact.lastName}
+        </h3>
+        <p className="text">{contact.phone}</p>
+        <p className="text">{contact.email}</p>
+      </div>
+    </article>
+  );
+};
+
+export default App;
