e1a4f08cd07e57dfe36568b763fa9bf95aa35e6e
Abdallah Sow <abdallahsow700@gmail.com>
2022-09-22 14:32:53 +0300

    made the button interactive to the darkMode state

--- projects/src/Project 1 Fun facts about React/App.js
+++ projects/src/Project 1 Fun facts about React/App.js
@@ -1,14 +1,25 @@
 import "./App.css";
 import Navbar from "./Components/Navbar";
 import Main from "./Components/Main";
+import { useState } from "react";
 
-const App = () => (
-  <div className="app">
-    <div className="container">
-      <Navbar />
-      <Main />
+const App = () => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const app = darkMode ? "app app-dark" : "app";
+
+  function change() {
+    setDarkMode(!darkMode);
+  }
+
+  return (
+    <div className={app}>
+      <div className="container">
+        <Navbar state={darkMode} handleClick={change} />
+        <Main state={darkMode}/>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default App;
--- projects/src/Project 1 Fun facts about React/Components/Navbar.js
+++ projects/src/Project 1 Fun facts about React/Components/Navbar.js
@@ -1,18 +1,30 @@
 import logo from "../images/react-logo.png";
 
-export default function Navbar() {
+export default function Navbar(props) {
+  const { state, handleClick } = props;
+
+  const classNames = {
+    light: state ? "light light-dark__mode" : "light",
+    dark: state ? "dark dark-dark__mode" : "dark",
+    change: state ? "toggle-switch toggle-switch-dark" : "toggle-switch",
+    navbar: state ? "navbar navbar-dark" : "navbar",
+    circle: state ? "switch__circle switch__circle-dark" : "switch__circle",
+  };
+
+  const { light, dark, change, navbar, circle } = classNames;
+
   return (
-    <nav className="navbar">
+    <nav className={navbar}>
       <div className="main-logo">
         <img className="react-logo" src={logo} alt="" />
         <h3 className="logo-title">React Facts</h3>
       </div>
       <div className="toggle">
-        <p className="light">Light</p>
-        <div className="toggle-switch">
-          <div className="switch__circle"></div>
+        <p className={light}>Light</p>
+        <div className={change} onClick={handleClick}>
+          <div className={circle}></div>
         </div>
-        <p className="dark">Dark</p>
+        <p className={dark}>Dark</p>
       </div>
     </nav>
   );
