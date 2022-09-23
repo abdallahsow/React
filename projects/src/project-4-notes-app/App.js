85623cd6a8cb626891e2681a911cd1de28a066ec
Abdallah Sow <abdallahsow700@gmail.com>
2022-09-23 10:25:29 +0300

    installed react-split package

--- projects/src/project-4-notes-app/App.js
+++ projects/src/project-4-notes-app/App.js
@@ -1,17 +1,26 @@
 import { useState } from "react";
 import "./App.css";
-import slipt from 'react-split';
-import { nanoid } from 'nanoid';
+// import slipt from "react-split";
+import { nanoid } from "nanoid";
 
 export default function App() {
-    const [notes, setNotes] = useState([])
-    
-    const createNewNote = () => {}
-  
-    return (
+  const [notes, setNotes] = useState([]);
+
+  const createNewNote = () => {
+    const newNote = {
+      id: nanoid(),
+      body: `Type here...`,
+    };
+
+    setNotes([newNote, ...notes]);
+  };
+
+  return (
     <div className="no-notes">
       <h1>You have no notes</h1>
-      <button className="first-note">Create one now</button>
+      <button onClick={createNewNote} className="first-note">
+        Create one now
+      </button>
     </div>
   );
 }
