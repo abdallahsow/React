319ef7ee2ffe5a764f82ad0b01c5bd191e829d0b
Abdallah Sow <abdallahsow700@gmail.com>
2022-09-26 11:58:48 +0300

    implemented the tenzies game project

--- projects/src/index.js
+++ projects/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./project-4-notes-app/App";
+import App from "./project-4-tenzies/App";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
---
+++ projects/src/project-4-tenzies/components/Header.js
@@ -0,0 +1,9 @@
+const Header = function () {
+    return (
+        <header>
+            <h1>Tenzies</h1>
+        </header>
+    )
+}
+
+export default Header
