c8cf74863b4a4531464c36ada35291bfd3956f99
Abdallah Sow <abdallahsow700@gmail.com>
2022-10-04 17:09:28 +0300

    refactored the code

--- src/project-4-tenzies/components/Dice.js
+++ src/project-4-tenzies/components/Dice.js
@@ -1,8 +1,13 @@
 const Dice = function ({ handleClick, value, isHeld, id }) {
+  const styles = {
+    backgroundColor: isHeld ? "#59E391" : "#fff",
+  };
+  
   return (
     <div
-      className={isHeld ? "dice-box dice-box-clicked" : "dice-box"}
+      className="dice-box"
       onClick={() => handleClick(id)}
+      style={styles}
     >
       <p className="dice">{value}</p>
     </div>
