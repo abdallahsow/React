f11aef285f88aa5861132e567046adaa8a3c626b
Abdallah Sow <abdallahsow700@gmail.com>
2022-10-09 18:04:35 +0300

    added an onclick event listener to change styles

--- src/project-5-quizzical-app/components/QuizPage/QuizPage.js
+++ src/project-5-quizzical-app/components/QuizPage/QuizPage.js
@@ -1,16 +1,37 @@
+import { useState } from "react";
 import Answer from "../Answer/Answer";
 import { quizData } from "../../data/quizData";
 import styles from "./QuizPage.module.css";
 
 export default function QuizPage() {
+  const [isClicked, setIsClicked] = useState(false);
+
+  function chooseAnswer(item) {
+    setIsClicked(() => {
+      for (const quiz of quizData) {
+        const { answers } = quiz;
+        for (const answer of answers) {
+          if (answer === item) {
+            return !isClicked;
+          }
+        }
+      }
+    });
+  }
+
   return (
     <div className={styles.quiz}>
       {quizData.map(({ question, answers }, index) => (
         <div key={index} className={styles.quizBox}>
-          <h4>{question}</h4>
+          <h4 className={styles.quizHeader}>{question}</h4>
           <div>
             {answers.map((answer, index) => (
-              <Answer key={index} answer={answer} />
+              <Answer
+                key={index}
+                answer={answer}
+                isClicked={isClicked}
+                handleClick={chooseAnswer}
+              />
             ))}
           </div>
           <hr />
