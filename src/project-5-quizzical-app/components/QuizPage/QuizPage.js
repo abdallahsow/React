bb3a4121c9873551aae6ef89e584ebf07038c50b
Abdallah Sow <abdallahsow700@gmail.com>
2022-10-18 12:12:59 +0300

    shuffled the answers' array

--- src/project-5-quizzical-app/components/QuizPage/QuizPage.js
+++ src/project-5-quizzical-app/components/QuizPage/QuizPage.js
@@ -4,16 +4,35 @@ import styles from "./QuizPage.module.css";
 export default function QuizPage() {
   const [quizData, setQuizData] = useState([]);
 
+  const shuffle = (array) => {
+    let currentIndex = array.length,
+      temporaryValue,
+      randomIndex;
+
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+  };
+
   const fetchData = async () => {
     const data = await fetch(
       "https://opentdb.com/api.php?amount=5&difficulty=medium&type=multiple"
     );
     const { results } = await data.json();
+    console.log(results);
     const quiz = [];
     for (const result of results) {
       const { question, correct_answer, incorrect_answers } = result;
       const answers = [...incorrect_answers, correct_answer];
-      quiz.push({ question, answers });
+      const shuffledAnswers = shuffle(answers);
+      quiz.push({ question, shuffledAnswers });
     }
     setQuizData(quiz);
   };
@@ -26,11 +45,11 @@ export default function QuizPage() {
 
   return (
     <div className={styles.quiz}>
-      {quizData.map(({ question, answers }, index) => (
+      {quizData.map(({ question, shuffledAnswers }, index) => (
         <div key={index} className={styles.quizBox}>
           <h4 className={styles.quizHeader}>{question}</h4>
           <div>
-            {answers.map((answer, index) => (
+            {shuffledAnswers.map((answer, index) => (
               <button key={index} className={styles.singleAnswer}>
                 {answer}
               </button>
